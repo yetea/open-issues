e395ddb02cd8648c833b6983337945a5c7cfecc0
yetea <cmuhorakeye16@gmail.com>
2023-04-10 14:56:55 +0200

    chore: add engine-strict to .npmrc feat: add engines field to package.json with node, pnpm, yarn, and npm versions feat(index.tsx): wrap h1 element in a fragment to allow for future additions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
-
 export default function Home() {
   return (
-    <h1 className='text-3xl underline font-bold text-red-500'>Hello world!</h1>
+    <>
+      <h1 className='text-3xl underline font-bold text-red-500'>Hello world!</h1>
+    </>
   )
 }
